290555b3e0bd5c781b86627402c26d707b20bde5
joana <joanailc@gmail.com>
2020-04-22 17:32:31 +0100

    Initial setup of restapi get method to retrieve a filtered image from a passed public URL.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -13,9 +13,9 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
+  // GET a filtered image passing a public URL --> /filteredimage?image_url={{URL}}
+  // Try locally - happy flow: http://localhost:8082/filteredimage?image_url=https://exitoina.uol.com.br/media/_versions/babyyoda309850_widelg.jpg
+  // Try locally - error: http://localhost:8082/filteredimage?image_url=https://timedotcom.files.wordpress.com/2019/03/kitten-report.jpg
   // IT SHOULD
   //    1
   //    1. validate the image_url query
@@ -26,6 +26,29 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get( "/filteredimage/", async ( req: Request, res: Response ) => {
+    let { image_url } = req.query;
+
+    //validate the image_url query
+    if ( !image_url ) {
+      return res.status(400)
+          .send(`Image URL is required!`);
+    }
+
+    //call filterImageFromURL(image_url) to filter the image
+    let filteredImage = await filterImageFromURL(image_url);
+
+    //check if there is a response
+    if(!filteredImage) {
+      return res.status(400)
+          .send(`Error! Not possible to filter the provided image!`);
+    }
+
+    return res.status(200)
+        .sendFile(filteredImage, res.status(400)
+            .send(`Error! Not possible to filter the provided image!`));
+
+  } );
 
   /**************************************************************************** */
 
